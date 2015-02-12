be94caa5b403dda73e3ae71ccbfd5ba600ced428
xroca <xroca21@gmail.com>
2015-02-12 16:50:53 +0100

    Pujar projecte

---
+++ github_jgassoroca/src/github_jgassoroca/Principal.java
@@ -0,0 +1,13 @@
+package github_jgassoroca;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("GitHub del Joel Gassó i el Xavier Roca");
+		
+	}
+
+}
